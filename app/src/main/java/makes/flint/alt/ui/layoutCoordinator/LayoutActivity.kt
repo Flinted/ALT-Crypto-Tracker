55fcb4cb744fd8467c2f600671624d8a22f83c5b
Chris Flint <chris.flint@intelligentpos.com>
2018-03-09 12:19:27 +0000

    Move all constraint adjustments to XML

--- app/src/main/java/makes/flint/alt/ui/layoutCoordinator/LayoutActivity.kt
+++ app/src/main/java/makes/flint/alt/ui/layoutCoordinator/LayoutActivity.kt
@@ -3,10 +3,7 @@ package makes.flint.alt.ui.layoutCoordinator
 import android.os.Bundle
 import android.os.Handler
 import android.support.constraint.ConstraintLayout
-import android.support.constraint.ConstraintSet
 import android.support.design.widget.FloatingActionButton
-import android.transition.TransitionManager
-import android.view.View
 import android.widget.FrameLayout
 import makes.flint.alt.R
 import makes.flint.alt.base.BaseActivity
@@ -25,21 +22,16 @@ class LayoutActivity : BaseActivity() {
     private lateinit var popFrameB: FrameLayout
     private lateinit var fab: FloatingActionButton
 
-    private lateinit var currentConstraintSet: ConstraintSet
-    private var constraintInitialising: ConstraintSet = ConstraintSet()
-    private var constraintHome: ConstraintSet = ConstraintSet()
-    private var constraintCoin: ConstraintSet = ConstraintSet()
-    private var constraintAdd: ConstraintSet = ConstraintSet()
-    private var constraintPortfolio: ConstraintSet = ConstraintSet()
+    private lateinit var coordinator: LayoutCoordinator
+    private var currentViewState = loading
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_template)
         bindViews()
-        setConstraintSets()
-        configureHomeConstraint()
-        configureCoinConstraint()
         setOnClick()
+        coordinator = LayoutCoordinator()
+        coordinator.initialiseConstraintsFor(masterLayout)
         loadMarketFragment()
     }
 
@@ -51,69 +43,14 @@ class LayoutActivity : BaseActivity() {
 
     private fun setOnClick() {
         fab.setOnClickListener {
-            val newConstraint = when (currentConstraintSet) {
-                constraintHome -> constraintCoin
-                else -> constraintHome
+            val viewKey = when (currentViewState) {
+                loading -> home
+                home -> coin
+                else -> home
             }
-            currentConstraintSet = newConstraint
-            TransitionManager.beginDelayedTransition(masterLayout)
-            newConstraint.applyTo(masterLayout)
+            currentViewState = viewKey
+            coordinator.changeConstraints(viewKey, masterLayout)
         }
-    }
-
-    private fun configureCoinConstraint() {
-        constraintCoin.apply {
-            connect(R.id.layout_fab, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP)
-            setVisibility(R.id.frame_b, View.GONE)
-            clear(R.id.frame_a)
-            connect(R.id.frame_a, ConstraintSet.BOTTOM, R.id.frame_c, ConstraintSet.TOP)
-            connect(R.id.frame_a, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP)
-            connect(R.id.frame_a, ConstraintSet.START, ConstraintSet.PARENT_ID, ConstraintSet.START)
-            connect(R.id.frame_a, ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END)
-            connect(R.id.pop_frame_b, ConstraintSet.TOP, R.id.frame_c, ConstraintSet.TOP, 50)
-            setMargin(R.id.pop_frame_b, ConstraintSet.BOTTOM, 50)
-            setMargin(R.id.pop_frame_b, ConstraintSet.START, 50)
-            setMargin(R.id.pop_frame_b, ConstraintSet.END, 50)
-            setElevation(R.id.pop_frame_b, 10f)
-            setVisibility(R.id.pop_frame_b, View.VISIBLE)
-        }
-    }
-
-    private fun configureHomeConstraint() {
-        constraintHome.apply {
-            clear(R.id.frame_a)
-            clear(R.id.frame_b)
-            clear(R.id.frame_c)
-
-            constrainHeight(R.id.frame_a, 300)
-            connect(R.id.frame_a, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP)
-            connect(R.id.frame_a, ConstraintSet.START, ConstraintSet.PARENT_ID, ConstraintSet.START)
-            connect(R.id.frame_a, ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END)
-
-            constrainHeight(R.id.frame_c, 700)
-            connect(R.id.frame_c, ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM)
-            connect(R.id.frame_c, ConstraintSet.START, ConstraintSet.PARENT_ID, ConstraintSet.START)
-            connect(R.id.frame_c, ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END)
-
-            connect(R.id.frame_b, ConstraintSet.BOTTOM, R.id.frame_c, ConstraintSet.TOP)
-            connect(R.id.frame_b, ConstraintSet.TOP, R.id.frame_a, ConstraintSet.BOTTOM)
-            connect(R.id.frame_b, ConstraintSet.START, ConstraintSet.PARENT_ID, ConstraintSet.START)
-            connect(R.id.frame_b, ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END)
-
-            setVisibility(R.id.pop_frame_a, View.GONE)
-            setVisibility(R.id.pop_frame_b, View.GONE)
-            setVisibility(R.id.layout_fab, View.VISIBLE)
-        }
-    }
-
-    private fun setConstraintSets() {
-        constraintInitialising.clone(masterLayout)
-        constraintHome.clone(masterLayout)
-        configureHomeConstraint()
-        constraintCoin.clone(constraintHome)
-        constraintAdd.clone(constraintHome)
-        constraintPortfolio.clone(constraintHome)
-        currentConstraintSet = constraintInitialising
     }
 
     private fun bindViews() {
--- app/src/main/java/makes/flint/alt/ui/layoutCoordinator/LayoutCoordinator.kt
+++ app/src/main/java/makes/flint/alt/ui/layoutCoordinator/LayoutCoordinator.kt
@@ -1,12 +1,44 @@
 package makes.flint.alt.ui.layoutCoordinator
 
+import android.support.constraint.ConstraintLayout
+import android.support.constraint.ConstraintSet
+import android.transition.TransitionManager
+import makes.flint.alt.R
+
 /**
  * LayoutCoordinator
  * Copyright © 2018 Intelligent Loyalty Limited. All rights reserved.
  */
+const val loading = "LOADING"
+const val home = "HOME"
+const val coin = "COIN"
+const val add = "ADD"
+const val portfolio = "PORTFOLIO"
+
 class LayoutCoordinator {
 
-    var layouts: MutableMap<String, LayoutSet> = mutableMapOf()
+    var layouts: MutableMap<String, ConstraintSet> = makeConstraintSetsMap()
+
+    private fun makeConstraintSetsMap() = mutableMapOf(
+            loading to ConstraintSet(),
+            home to ConstraintSet(),
+            coin to ConstraintSet(),
+            add to ConstraintSet(),
+            portfolio to ConstraintSet()
+    )
+
+    internal fun initialiseConstraintsFor(layout: ConstraintLayout) {
+        val context = layout.context
+        layouts[loading]?.clone(layout)
+        layouts[home]?.clone(context, R.layout.constraint_home)
+        layouts[coin]?.clone(context, R.layout.constraint_coin)
+    }
+
+    fun changeConstraints(viewKey: String, masterLayout: ConstraintLayout) {
+        val constraint = layouts[viewKey] ?: return
+        TransitionManager.beginDelayedTransition(masterLayout)
+        constraint.applyTo(masterLayout)
+    }
 
 
 }
